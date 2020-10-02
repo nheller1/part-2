9fb5e67462b78625fffc4481ee8a305e8850b662
Nikida Heller <53826007+nheller1@users.noreply.github.com>
2020-10-02 11:42:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+let rain = prompt ("How many inches of rain fell?");
+let inches = "*";
+console.log("Rainfall: " + inches.repeat(rain));
+
+let fertilizer = prompt("Did you use fertilizer?");
+if (fertilizer ==="no"){
+    (inches<= light rain);
+  console.log("The yield should be " + (50*.8) + " per year.");
+}
+if (fertilizer ==="no") {
+    (inches <= heavyrain)
+      console.log(The yield should be" + (50.9) + "per year.")
+  
+  
+  console.log("the yield should be" +BLANK+ "per year.");
+}
+
+if (fertilizer === "yes") {
+  
+  let brand = prompt("Did you use premium or regular fertilizer?");
+  
+  
+}
+console.log(result);
